f325570eea8308da747b0cf4393456c5773f4be3
NeilRow56 <neilrowland56@gmail.com>
2025-01-27 17:30:36 +0000

    Sign in and sign out functions working. User button added to header menu

--- src/components/shared/header/menu.tsx
+++ src/components/shared/header/menu.tsx
@@ -9,6 +9,7 @@ import {
   SheetTitle,
   SheetTrigger
 } from '@/components/ui/sheet'
+import UserButton from './user-button'
 
 const Menu = () => {
   return (
@@ -20,12 +21,8 @@ const Menu = () => {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button variant='default' asChild>
-          <Link href='/sign-in'>
-            <User2Icon />
-            Sign in
-          </Link>
-        </Button>
+
+        <UserButton />
       </nav>
       <nav className='md:hidden'>
         <Sheet>
@@ -40,12 +37,8 @@ const Menu = () => {
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button variant='default' asChild>
-              <Link href='/sign-in'>
-                <User2Icon />
-                Sign in
-              </Link>
-            </Button>
+
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
