8426fec203d120272e51d691e6812663af648bb1
NeilRow56 <neilrowland56@gmail.com>
2025-01-28 04:34:37 +0000

    Sign up error handing

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signInFormSchema, signUpFormSchema } from '../validators'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { prisma } from '@/db/prisma'
 import { hashSync } from 'bcrypt-ts-edge'
+import { formatError } from '../utils'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -61,6 +62,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
     if (isRedirectError(error)) {
       throw error
     }
-    return { success: false, message: 'User was not registered' }
+    return { success: false, message: formatError(error) }
   }
 }
