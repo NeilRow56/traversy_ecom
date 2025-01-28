a8e8e269f2199b72c7a45a3508dc13665757ed21
NeilRow56 <neilrowland56@gmail.com>
2025-01-28 15:01:14 +0000

    Added session cookie Id to auth.js and authConfig etc

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 import { prisma } from '@/db/prisma'
+import { cookies } from 'next/headers'
 import { compareSync } from 'bcrypt-ts-edge'
-
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const config = {
@@ -82,6 +82,30 @@ export const config = {
             where: { id: user.id },
             data: { name: token.name }
           })
+        }
+
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies()
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId }
+            })
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id }
+              })
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id }
+              })
+            }
+          }
         }
       }
 
