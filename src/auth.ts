51ed6287acd87fb80255711b4943f1f2c0774170
NeilRow56 <neilrowland56@gmail.com>
2025-01-29 05:21:35 +0000

    Update addItemToCart action. getMyCart and calculate cart price function added

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 import { prisma } from '@/db/prisma'
-import { cookies } from 'next/headers'
 import { compareSync } from 'bcrypt-ts-edge'
+
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { cookies } from 'next/headers'
 
 export const config = {
   pages: {
@@ -83,29 +84,28 @@ export const config = {
             data: { name: token.name }
           })
         }
-
         if (trigger === 'signIn' || trigger === 'signUp') {
           const cookiesObject = await cookies()
           const sessionCartId = cookiesObject.get('sessionCartId')?.value
 
-          if (sessionCartId) {
-            const sessionCart = await prisma.cart.findFirst({
-              where: { sessionCartId }
-            })
+          // if (sessionCartId) {
+          //   const sessionCart = await prisma.cart.findFirst({
+          //     where: { sessionCartId },
+          //   });
 
-            if (sessionCart) {
-              // Delete current user cart
-              await prisma.cart.deleteMany({
-                where: { userId: user.id }
-              })
+          //   if (sessionCart) {
+          //     // Delete current user cart
+          //     await prisma.cart.deleteMany({
+          //       where: { userId: user.id },
+          //     });
 
-              // Assign new cart
-              await prisma.cart.update({
-                where: { id: sessionCart.id },
-                data: { userId: user.id }
-              })
-            }
-          }
+          //     // Assign new cart
+          //     await prisma.cart.update({
+          //       where: { id: sessionCart.id },
+          //       data: { userId: user.id },
+          //     });
+          //   }
+          // }
         }
       }
 
