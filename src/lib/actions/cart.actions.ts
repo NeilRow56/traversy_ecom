10623bb8e7b71289a17a393be85f6340da7cdb69
NeilRow56 <neilrowland56@gmail.com>
2025-01-29 06:09:30 +0000

    Handle quatity and mutiple products

--- src/lib/actions/cart.actions.ts
+++ src/lib/actions/cart.actions.ts
@@ -1,13 +1,13 @@
 'use server'
 
+import { cookies } from 'next/headers'
 import { CartItem } from '@/types'
 import { convertToPlainObject, formatError, round2 } from '../utils'
-
 import { auth } from '@/auth'
-import { cookies } from 'next/headers'
 import { prisma } from '@/db/prisma'
 import { cartItemSchema, insertCartSchema } from '../validators'
 import { revalidatePath } from 'next/cache'
+import { Prisma } from '@prisma/client'
 
 // Calculate cart prices
 const calcPrice = (items: CartItem[]) => {
@@ -46,7 +46,6 @@ export async function addItemToCart(data: CartItem) {
     const product = await prisma.product.findFirst({
       where: { id: item.productId }
     })
-
     if (!product) throw new Error('Product not found')
 
     if (!cart) {
@@ -68,15 +67,52 @@ export async function addItemToCart(data: CartItem) {
 
       return {
         success: true,
-        message: 'Item added to cart'
+        message: `${product.name} added to cart`
       }
     } else {
       // Check if item is already in cart
+      const existItem = (cart.items as CartItem[]).find(
+        x => x.productId === item.productId
+      )
+
+      if (existItem) {
+        // Check stock
+        if (product.stock < existItem.qty + 1) {
+          throw new Error('Not enough stock')
+        }
+
+        // Increase the quantity
+        ;(cart.items as CartItem[]).find(
+          x => x.productId === item.productId
+        )!.qty = existItem.qty + 1
+      } else {
+        // If item does not exist in cart
+        // Check stock
+        if (product.stock < 1) throw new Error('Not enough stock')
+
+        // Add item to the cart.items
+        cart.items.push(item)
+      }
+
+      // Save to database
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: cart.items as Prisma.CartUpdateitemsInput[],
+          ...calcPrice(cart.items as CartItem[])
+        }
+      })
+
+      revalidatePath(`/product/${product.slug}`)
+
+      return {
+        success: true,
+        message: `${product.name} ${existItem ? 'updated in' : 'added to'} cart`
+      }
     }
   } catch (error) {
     return {
       success: false,
-
       message: formatError(error)
     }
   }
