8f9318f9ca294b111299d7b5de6b24f0660866f2
NeilRow56 <neilrowland56@gmail.com>
2025-02-05 17:30:28 +0000

    Completed Product form and individual product update

---
+++ src/app/admin/products/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { getProductById } from '@/lib/actions/product.actions'
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import ProductForm from '../create/product-form'
+
+export const metadata: Metadata = {
+  title: 'Update Product'
+}
+
+const AdminProductUpdatePage = async (props: {
+  params: Promise<{
+    id: string
+  }>
+}) => {
+  const { id } = await props.params
+
+  const product = await getProductById(id)
+
+  if (!product) return notFound()
+
+  return (
+    <div className='mx-auto max-w-5xl space-y-8'>
+      <h1 className='h2-bold'>Update Product</h1>
+
+      <ProductForm type='Update' product={product} productId={product.id} />
+    </div>
+  )
+}
+
+export default AdminProductUpdatePage
