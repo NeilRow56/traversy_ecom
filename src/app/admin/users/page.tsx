dfe8be7084338465a8d689cc29b446529f44b429
NeilRow56 <neilrowland56@gmail.com>
2025-02-06 07:35:07 +0000

    Added users table and actions for getAllUsers and deleteUser

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,9 +1,79 @@
-const AdninUsersPage = () => {
+import { Metadata } from 'next'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+import { formatId } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import Pagination from '@/components/shared/pagination'
+import { Badge } from '@/components/ui/badge'
+import DeleteDialog from '@/components/shared/delete-dialog'
+import { deleteUser, getAllUsers } from '@/lib/actions/user.actions'
+
+export const metadata: Metadata = {
+  title: 'Admin Users'
+}
+
+const AdminUserPage = async (props: {
+  searchParams: Promise<{
+    page: string
+  }>
+}) => {
+  const { page = '1' } = await props.searchParams
+
+  const users = await getAllUsers({ page: Number(page) })
+
   return (
-    <div>
-      <h1>Admin users page</h1>
+    <div className='space-y-2'>
+      <div className='flex items-center gap-3'>
+        <h1 className='h2-bold'>Users</h1>
+      </div>
+      <div className='overflow-x-auto'>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>NAME</TableHead>
+              <TableHead>EMAIL</TableHead>
+              <TableHead>ROLE</TableHead>
+              <TableHead>ACTIONS</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.data.map(user => (
+              <TableRow key={user.id}>
+                <TableCell>{formatId(user.id)}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  {user.role === 'user' ? (
+                    <Badge variant='secondary'>User</Badge>
+                  ) : (
+                    <Badge variant='default'>Admin</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <Button asChild variant='outline' size='sm'>
+                    <Link href={`/admin/users/${user.id}`}>Edit</Link>
+                  </Button>
+                  <DeleteDialog id={user.id} action={deleteUser} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {users.totalPages > 1 && (
+          <Pagination page={Number(page) || 1} totalPages={users?.totalPages} />
+        )}
+      </div>
     </div>
   )
 }
 
-export default AdninUsersPage
+export default AdminUserPage
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -13,6 +13,9 @@ import { hashSync } from 'bcrypt-ts-edge'
 import { formatError } from '../utils'
 import { ShippingAddress } from '@/types'
 import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import { Prisma } from '@prisma/client'
+import { PAGE_SIZE } from '../constants'
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -165,5 +168,46 @@ export async function updateProfile(user: { name: string; email: string }) {
     }
   } catch (error) {
     return { success: false, message: formatError(error) }
+  }
+}
+
+// Get all the users
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page
+}: {
+  limit?: number
+  page: number
+}) {
+  const data = await prisma.user.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * limit
+  })
+
+  const dataCount = await prisma.user.count()
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit)
+  }
+}
+
+// Delete a user
+export async function deleteUser(id: string) {
+  try {
+    await prisma.user.delete({ where: { id } })
+
+    revalidatePath('/admin/users')
+
+    return {
+      success: true,
+      message: 'User deleted successfully'
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error)
+    }
   }
 }
