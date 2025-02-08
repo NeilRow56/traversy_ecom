cd21f7e4cd2f66cd7c536e6cc9fbd137ad358f4c
NeilRow56 <neilrowland56@gmail.com>
2025-02-08 14:48:24 +0000

    Added sorting to search page

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -28,6 +28,8 @@ const prices = [
 
 const ratings = [4, 3, 2, 1]
 
+const sortOrders = ['newest', 'lowest', 'highest', 'rating']
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string
@@ -177,6 +179,19 @@ const SearchPage = async (props: {
                 <Link href='/search'>Clear</Link>
               </Button>
             ) : null}
+          </div>
+          {/* Sort */}
+          <div>
+            Sort by{' '}
+            {sortOrders.map(s => (
+              <Link
+                key={s}
+                className={`mx-2 ${sort == s && 'font-bold'}`}
+                href={getFilterUrl({ s })}
+              >
+                {s}
+              </Link>
+            ))}
           </div>
         </div>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
